87b42107c7f4eb6a332b913ff6b412cb1de7bf4c
pav6778 <panchukovp@gmail.com>
2019-12-06 14:02:50 -0500

    set up mdl portfolio template

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+
+var app = express();
+
+var PORT = process.env.PORT || 3000
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+require('./routes/htmlRoutes')
+
+app.listen(PORT, function(){
+    console.log('Server is listening on port ' + PORT)
+})
