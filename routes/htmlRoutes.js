2b6926e70c38372c90bf655d40881aa990b3ec32
pav6778 <panchukovp@gmail.com>
2019-12-08 13:50:10 -0500

    made changes to htmlroutes.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,5 +1,20 @@
+var path = require("path");
+
+
 module.exports = function(app){
     app.get('/', function(req,res){
-        res.render('index')
+        res.sendFile(path.join(__dirname, "../public/index.html"))
+    })
+    app.get('/about', function(req,res){
+        res.sendFile(path.join(__dirname, "../public/about.html"))
+    })
+    app.get('/blog', function(req,res){
+        res.sendFile(path.join(__dirname, "../public/blog.html"))
+    })
+    app.get('/contact', function(req,res){
+        res.sendFile(path.join(__dirname, "../public/contact.html"))
+    })
+    app.get('/portfolio', function(req,res){
+        res.sendFile(path.join(__dirname, "../public/portfolio.html"))
     })
 }
